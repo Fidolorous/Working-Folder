0be4d5bf17c04ce5972aec8edb25f813c9398105
Fidolorous <fidolorous@hotmail.com>
2024-07-06 12:47:47 +0200

    commit  -a

---
+++ spider.js
@@ -0,0 +1,17 @@
+spiderJson = {"nodes":[
+{"weight":2,"rank":15.199999999999996, "id":1, "url":"http://www.dr-chuck.com"},
+{"weight":2,"rank":19.0, "id":2, "url":"http://www.dr-chuck.com/dr-chuck/resume/index.htm"},
+{"weight":1,"rank":7.599999999999983, "id":3, "url":"http://www.dr-chuck.com/office"},
+{"weight":1,"rank":3.799999999999999, "id":4, "url":"http://www.dr-chuck.com/sakai-book"},
+{"weight":1,"rank":0.0, "id":15, "url":"http://www.dr-chuck.com/dr-chuck/resume/speaking.htm"},
+{"weight":2,"rank":0.0, "id":17, "url":"http://www.dr-chuck.com/dr-chuck/resume"}],
+"links":[
+{"source":0,"target":1,"value":3},
+{"source":0,"target":2,"value":3},
+{"source":0,"target":3,"value":3},
+{"source":1,"target":0,"value":3},
+{"source":1,"target":4,"value":3},
+{"source":1,"target":5,"value":3},
+{"source":4,"target":1,"value":3},
+{"source":5,"target":0,"value":3},
+{"source":5,"target":5,"value":3}]};
